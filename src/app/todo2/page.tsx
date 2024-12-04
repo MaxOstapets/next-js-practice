e6b14a7793a8a4b515ac01cdd861c72a684473a4
Max Ostapets <maksostapets2007@gmail.com>
2024-12-05 00:14:06 +0200

    started making design

--- src/app/todo2/page.tsx
+++ src/app/todo2/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useReducer, useState } from "react";
+import { BiCheck } from "react-icons/bi";
 
 interface IState {
     list: { id: number; task: string; isComplete: boolean; score: number }[];
@@ -56,14 +57,47 @@ const List = () => {
     };
 
     return (
-        <section className="flex justify-center items-center flex-col gap-5">
-            <span className="text-7xl">Name: </span>
-            <div className="flex justify-center items-center flex-col ">
-                <p className="text-6xl">Your score: {state.allScore}</p>
-                <p className="text-6xl">Your tasks:</p>
-                <ul className="flex justify-center items-center flex-col gap-1 text-4xl">
+        <section className="flex justify-around gap-5 mt-24">
+           
+            <div className="flex flex-col gap-8 bg-blue-900 p-6 text-blue-900 rounded-lg h-72">
+                <ul className="flex flex-col gap-1 text-white">
+                    <li className="text-5xl">Name: </li>
+                    <li className="text-4xl">Your score: {state.allScore}</li>
+                </ul>
+                <div className="flex flex-col gap-2">
+                    <div className="flex gap-1">
+                        <input
+                            type="text"
+                            placeholder="Write your task"
+                            className="w-96 h-10 border-2 text-xl p-2 border-white"
+                            value={task}
+                            onChange={(e) => setTask(e.target.value)}
+                        />
+                        <button
+                            className="w-10 h-10 border-2 text-2xl border-white bg-white"
+                            onClick={addTask}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className="flex gap-1">
+                        <input
+                            type="text"
+                            placeholder="Write your name"
+                            className="w-96 h-10 border-2 text-xl p-2 border-white"
+                        />
+                        <button className="flex justify-center items-center w-10 h-10 border-2 text-2xl border-white bg-white">
+                            <BiCheck />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center flex-col bg-blue-900 h-96 snap-y w-1/3">
+                <p className="text-5xl text-white">Your tasks:</p>
+                <ul className="flex justify-center items-center flex-col gap-1 text-2xl">
                     {state.list.map((el) => (
-                        <li key={el.id}>
+                        <li key={el.id} className="w-80 bg-white flex justify-between px-2 items-center">
                             <span>{el.task}</span>
                             <input
                                 type="checkbox"
@@ -73,21 +107,7 @@ const List = () => {
                     ))}
                 </ul>
             </div>
-            <div className="flex flex-col gap-3">
-                <input
-                    type="text"
-                    placeholder="task"
-                    className="w-96 h-10 border-2 text-2xl p-2 border-black"
-                    value={task}
-                    onChange={(e) => setTask(e.target.value)}
-                />
-            </div>
-            <button
-                className="w-96 h-10 border-2 text-2xl border-black font-bold"
-                onClick={addTask}
-            >
-                Add task
-            </button>
+
         </section>
     );
 };
